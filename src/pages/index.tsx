0561c4eb8339eeada36c9a60cb7f074b6aa8a86c
Nysreynit's Macbook pro <nysreynitsmacbookpro@Nysreynitss-MacBook-Pro.local>
2025-12-18 17:55:12 +0700

    cahnge tiktok

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -74,7 +74,7 @@ export default function Home() {
             </a>
             <a
               className={styles.primary}
-              href="#"
+              href="www.tiktok.com/@tholsopanha0"
               target="_blank"
               rel="noopener noreferrer"
             >
